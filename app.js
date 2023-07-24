4ff90277a18d43baaf32d759f255f05224fef0c3
Charles Renaud <57405542+CharlesRenaud@users.noreply.github.com>
2023-07-24 16:31:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const scriptRoutes = require('./routes/scripts');
 const app = express();
 
 // Connexion à la base de données MongoDB
-mongoose.connect(`mongodb+srv://crenaud:xK1wJmBNLKYN7wa1@serverlessinstance0.xagzb.mongodb.net/?retryWrites=true&w=majority`, {
+mongoose.connect(`URL_DB`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     keepAlive: true
